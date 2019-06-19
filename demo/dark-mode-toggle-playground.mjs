6c1e3d5a14b7b890c92c2c27a8d4e6bfaed194a8
tomayac <steiner.thomas@gmail.com>
2019-06-19 10:51:47 +0200

    Fix points brought up in #1

--- demo/dark-mode-toggle-playground.mjs
+++ demo/dark-mode-toggle-playground.mjs
@@ -1,5 +1,4 @@
 /**
- * @license
  * Copyright 2019 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 ((doc) => {
   const themeColor = doc.querySelector('meta[name="theme-color"]');
   const icon = doc.querySelector('link[rel="icon"]');
