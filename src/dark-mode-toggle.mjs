5be67678e2d59350606362e40020a8f69a776c48
Haocen Xu <admin@haocen.me>
2022-11-10 21:27:48 -0500

    Revert change to DEFAULT_URL

--- src/dark-mode-toggle.mjs
+++ src/dark-mode-toggle.mjs
@@ -42,7 +42,7 @@ const PERMANENT_COLOR_SCHEME = 'permanentcolorscheme';
 const ALL = 'all';
 const NOT_ALL = 'not all';
 const NAME = 'dark-mode-toggle';
-const DEFAULT_URL = 'https://haocen.github.io/dark-mode-toggle/demo/';
+const DEFAULT_URL = 'https://googlechromelabs.github.io/dark-mode-toggle/demo/';
 
 // See https://html.spec.whatwg.org/multipage/common-dom-interfaces.html ↵
 // #reflecting-content-attributes-in-idl-attributes.
