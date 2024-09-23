c258ae4bf50275520cd81d1f1ebf86c228625aa3
Thomas Steiner <tomac@google.com>
2024-09-23 10:39:54 +0200

    New version

--- src/dark-mode-toggle.mjs
+++ src/dark-mode-toggle.mjs
@@ -79,7 +79,7 @@ const installBoolReflection = (obj, attrName, propName = attrName) => {
 const template = doc.createElement('template');
 // ⚠️ Note: this is a minified version of `src/template-contents.tpl`.
 // Compress the CSS with https://cssminifier.com/, then paste it here.
-// eslint-disable-next-line max-len
+
 template.innerHTML = `<style>*,::after,::before{box-sizing:border-box}:host{contain:content;display:block}:host([hidden]){display:none}form{background-color:var(--${NAME}-background-color,transparent);color:var(--${NAME}-color,inherit);padding:0}fieldset{border:none;margin:0;padding-block:.25rem;padding-inline:.25rem}legend{font:var(--${NAME}-legend-font,inherit);padding:0}input,label{cursor:pointer}label{white-space:nowrap}input{opacity:0;position:absolute;pointer-events:none}input:focus-visible+label{outline:#e59700 auto 2px;outline:-webkit-focus-ring-color auto 5px}label::before{content:"";display:inline-block;background-size:var(--${NAME}-icon-size,1rem);background-repeat:no-repeat;height:var(--${NAME}-icon-size,1rem);width:var(--${NAME}-icon-size,1rem);vertical-align:middle}label:not(:empty)::before{margin-inline-end:.5rem}[part=lightLabel]::before,[part=lightThreeWayLabel]::before{background-image:var(--${NAME}-light-icon, url("${DEFAULT_URL}sun.png"))}[part=darkLabel]::before,[part=darkThreeWayLabel]::before{filter:var(--${NAME}-icon-filter, none);background-image:var(--${NAME}-dark-icon, url("${DEFAULT_URL}moon.png"))}[part=systemThreeWayLabel]::before{background-image:var(--${NAME}-system-icon, url("${DEFAULT_URL}system.png"))}[part=toggleLabel]::before{background-image:var(--${NAME}-checkbox-icon,none)}[part=permanentLabel]::before{background-image:var(--${NAME}-remember-icon-unchecked, url("${DEFAULT_URL}unchecked.svg"))}[part$=ThreeWayLabel],[part=darkLabel],[part=lightLabel],[part=toggleLabel]{font:var(--${NAME}-label-font,inherit)}[part$=ThreeWayLabel]:empty,[part=darkLabel]:empty,[part=lightLabel]:empty,[part=toggleLabel]:empty{font-size:0;padding:0}[part=permanentLabel]{font:var(--${NAME}-remember-font,inherit)}input:checked+[part=permanentLabel]::before{background-image:var(--${NAME}-remember-icon-checked, url("${DEFAULT_URL}checked.svg"))}input:checked+[part$=ThreeWayLabel],input:checked+[part=darkLabel],input:checked+[part=lightLabel]{background-color:var(--${NAME}-active-mode-background-color,transparent)}input:checked+[part$=ThreeWayLabel]::before,input:checked+[part=darkLabel]::before,input:checked+[part=lightLabel]::before{background-color:var(--${NAME}-active-mode-background-color,transparent)}input:checked+[part=toggleLabel]::before,input[part=toggleCheckbox]:checked~[part=threeWayRadioWrapper] [part$=ThreeWayLabel]::before{filter:var(--${NAME}-icon-filter, none)}input:checked+[part=toggleLabel]~aside [part=permanentLabel]::before{filter:var(--${NAME}-remember-filter, invert(100%))}aside{visibility:hidden;margin-block-start:.15rem}[part=darkLabel]:focus-visible~aside,[part=lightLabel]:focus-visible~aside,[part=toggleLabel]:focus-visible~aside{visibility:visible;transition:visibility 0s}aside [part=permanentLabel]:empty{display:none}@media (hover:hover){aside{transition:visibility 3s}aside:hover{visibility:visible}[part=darkLabel]:hover~aside,[part=lightLabel]:hover~aside,[part=toggleLabel]:hover~aside{visibility:visible;transition:visibility 0s}}</style><form part=form><fieldset part=fieldset><legend part=legend></legend><input id=l part=lightRadio type=radio name=mode> <label for=l part=lightLabel></label> <input id=d part=darkRadio type=radio name=mode> <label for=d part=darkLabel></label> <input id=t part=toggleCheckbox type=checkbox> <label for=t part=toggleLabel></label> <span part=threeWayRadioWrapper><input id=3l part=lightThreeWayRadio type=radio name=three-way-mode> <label for=3l part=lightThreeWayLabel></label> <input id=3s part=systemThreeWayRadio type=radio name=three-way-mode> <label for=3s part=systemThreeWayLabel></label> <input id=3d part=darkThreeWayRadio type=radio name=three-way-mode> <label for=3d part=darkThreeWayLabel></label></span><aside part=aside><input id=p part=permanentCheckbox type=checkbox> <label for=p part=permanentLabel></label></aside></fieldset></form>`;
 
 export class DarkModeToggle extends HTMLElement {
@@ -120,16 +120,16 @@ export class DarkModeToggle extends HTMLElement {
   }
 
   _initializeDOM() {
-    const shadowRoot = this.attachShadow({mode: 'open'});
+    const shadowRoot = this.attachShadow({ mode: 'open' });
     shadowRoot.append(template.content.cloneNode(true));
 
     // We need to support `media="(prefers-color-scheme: dark)"` (with space)
     // and `media="(prefers-color-scheme:dark)"` (without space)
     this._darkCSS = doc.querySelectorAll(
-        `${LINK_REL_STYLESHEET}[${MEDIA}*=${PREFERS_COLOR_SCHEME}][${MEDIA}*="${DARK}"]`,
+      `${LINK_REL_STYLESHEET}[${MEDIA}*=${PREFERS_COLOR_SCHEME}][${MEDIA}*="${DARK}"]`,
     );
     this._lightCSS = doc.querySelectorAll(
-        `${LINK_REL_STYLESHEET}[${MEDIA}*=${PREFERS_COLOR_SCHEME}][${MEDIA}*="${LIGHT}"]`,
+      `${LINK_REL_STYLESHEET}[${MEDIA}*=${PREFERS_COLOR_SCHEME}][${MEDIA}*="${LIGHT}"]`,
     );
 
     // Get DOM references.
@@ -140,27 +140,27 @@ export class DarkModeToggle extends HTMLElement {
     this._darkCheckbox = shadowRoot.querySelector('[part=toggleCheckbox]');
     this._checkboxLabel = shadowRoot.querySelector('[part=toggleLabel]');
     this._lightThreeWayRadio = shadowRoot.querySelector(
-        '[part=lightThreeWayRadio]',
+      '[part=lightThreeWayRadio]',
     );
     this._lightThreeWayLabel = shadowRoot.querySelector(
-        '[part=lightThreeWayLabel]',
+      '[part=lightThreeWayLabel]',
     );
     this._systemThreeWayRadio = shadowRoot.querySelector(
-        '[part=systemThreeWayRadio]',
+      '[part=systemThreeWayRadio]',
     );
     this._systemThreeWayLabel = shadowRoot.querySelector(
-        '[part=systemThreeWayLabel]',
+      '[part=systemThreeWayLabel]',
     );
     this._darkThreeWayRadio = shadowRoot.querySelector(
-        '[part=darkThreeWayRadio]',
+      '[part=darkThreeWayRadio]',
     );
     this._darkThreeWayLabel = shadowRoot.querySelector(
-        '[part=darkThreeWayLabel]',
+      '[part=darkThreeWayLabel]',
     );
     this._legendLabel = shadowRoot.querySelector('legend');
     this._permanentAside = shadowRoot.querySelector('aside');
     this._permanentCheckbox = shadowRoot.querySelector(
-        '[part=permanentCheckbox]',
+      '[part=permanentCheckbox]',
     );
     this._permanentLabel = shadowRoot.querySelector('[part=permanentLabel]');
   }
@@ -170,12 +170,12 @@ export class DarkModeToggle extends HTMLElement {
     const hasNativePrefersColorScheme = matchMedia(MQ_DARK).media !== NOT_ALL;
     // Listen to `prefers-color-scheme` changes.
     if (hasNativePrefersColorScheme) {
-      matchMedia(MQ_DARK).addListener(({matches}) => {
+      matchMedia(MQ_DARK).addListener(({ matches }) => {
         if (this.permanent) {
           return;
         }
         this.mode = matches ? DARK : LIGHT;
-        this._dispatchEvent(COLOR_SCHEME_CHANGE, {colorScheme: this.mode});
+        this._dispatchEvent(COLOR_SCHEME_CHANGE, { colorScheme: this.mode });
       });
     }
     // Set initial state, giving preference to a remembered value, then the
@@ -228,14 +228,14 @@ export class DarkModeToggle extends HTMLElement {
         this.mode = this._lightRadio.checked ? LIGHT : DARK;
         this._updateCheckbox();
         this._updateThreeWayRadios();
-        this._dispatchEvent(COLOR_SCHEME_CHANGE, {colorScheme: this.mode});
+        this._dispatchEvent(COLOR_SCHEME_CHANGE, { colorScheme: this.mode });
       });
     });
     this._darkCheckbox.addEventListener('change', () => {
       this.mode = this._darkCheckbox.checked ? DARK : LIGHT;
       this._updateRadios();
       this._updateThreeWayRadios();
-      this._dispatchEvent(COLOR_SCHEME_CHANGE, {colorScheme: this.mode});
+      this._dispatchEvent(COLOR_SCHEME_CHANGE, { colorScheme: this.mode });
     });
     this._lightThreeWayRadio.addEventListener('change', () => {
       this.mode = LIGHT;
@@ -243,7 +243,7 @@ export class DarkModeToggle extends HTMLElement {
       this._updateCheckbox();
       this._updateRadios();
       this._updateThreeWayRadios();
-      this._dispatchEvent(COLOR_SCHEME_CHANGE, {colorScheme: this.mode});
+      this._dispatchEvent(COLOR_SCHEME_CHANGE, { colorScheme: this.mode });
       this._dispatchEvent(PERMANENT_COLOR_SCHEME, {
         permanent: this.permanent,
       });
@@ -254,7 +254,7 @@ export class DarkModeToggle extends HTMLElement {
       this._updateCheckbox();
       this._updateRadios();
       this._updateThreeWayRadios();
-      this._dispatchEvent(COLOR_SCHEME_CHANGE, {colorScheme: this.mode});
+      this._dispatchEvent(COLOR_SCHEME_CHANGE, { colorScheme: this.mode });
       this._dispatchEvent(PERMANENT_COLOR_SCHEME, {
         permanent: this.permanent,
       });
@@ -265,7 +265,7 @@ export class DarkModeToggle extends HTMLElement {
       this._updateCheckbox();
       this._updateRadios();
       this._updateThreeWayRadios();
-      this._dispatchEvent(COLOR_SCHEME_CHANGE, {colorScheme: this.mode});
+      this._dispatchEvent(COLOR_SCHEME_CHANGE, { colorScheme: this.mode });
       this._dispatchEvent(PERMANENT_COLOR_SCHEME, {
         permanent: this.permanent,
       });
@@ -281,7 +281,7 @@ export class DarkModeToggle extends HTMLElement {
 
     // Finally update the mode and let the world know what's going on
     this._updateMode();
-    this._dispatchEvent(COLOR_SCHEME_CHANGE, {colorScheme: this.mode});
+    this._dispatchEvent(COLOR_SCHEME_CHANGE, { colorScheme: this.mode });
     this._dispatchEvent(PERMANENT_COLOR_SCHEME, {
       permanent: this.permanent,
     });
@@ -292,7 +292,7 @@ export class DarkModeToggle extends HTMLElement {
       const allAttributes = [LIGHT, SYSTEM, DARK];
       if (!allAttributes.includes(newValue)) {
         throw new RangeError(
-            `Allowed values are: "${allAttributes.join(`", "`)}".`,
+          `Allowed values are: "${allAttributes.join(`", "`)}".`,
         );
       }
       // Only show the dialog programmatically on devices not capable of hover
@@ -315,7 +315,7 @@ export class DarkModeToggle extends HTMLElement {
       const allAppearanceOptions = [TOGGLE, SWITCH, THREE_WAY];
       if (!allAppearanceOptions.includes(newValue)) {
         throw new RangeError(
-            `Allowed values are: "${allAppearanceOptions.join(`", "`)}".`,
+          `Allowed values are: "${allAppearanceOptions.join(`", "`)}".`,
         );
       }
       this._updateAppearance();
@@ -359,11 +359,11 @@ export class DarkModeToggle extends HTMLElement {
 
   _dispatchEvent(type, value) {
     this.dispatchEvent(
-        new CustomEvent(type, {
-          bubbles: true,
-          composed: true,
-          detail: value,
-        }),
+      new CustomEvent(type, {
+        bubbles: true,
+        composed: true,
+        detail: value,
+      }),
     );
   }
 
@@ -418,8 +418,8 @@ export class DarkModeToggle extends HTMLElement {
   _updateCheckbox() {
     if (this.mode === LIGHT) {
       this._checkboxLabel.style.setProperty(
-          `--${NAME}-checkbox-icon`,
-          `var(--${NAME}-light-icon,url("${DEFAULT_URL}moon.png"))`,
+        `--${NAME}-checkbox-icon`,
+        `var(--${NAME}-light-icon,url("${DEFAULT_URL}moon.png"))`,
       );
       this._checkboxLabel.textContent = this.light;
       if (!this.light) {
@@ -428,8 +428,8 @@ export class DarkModeToggle extends HTMLElement {
       this._darkCheckbox.checked = false;
     } else {
       this._checkboxLabel.style.setProperty(
-          `--${NAME}-checkbox-icon`,
-          `var(--${NAME}-dark-icon,url("${DEFAULT_URL}sun.png"))`,
+        `--${NAME}-checkbox-icon`,
+        `var(--${NAME}-dark-icon,url("${DEFAULT_URL}sun.png"))`,
       );
       this._checkboxLabel.textContent = this.dark;
       if (!this.dark) {
